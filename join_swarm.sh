ab81cad0c10bd63e41778ab3a0a846296d04a5d4
Mauricio Collazos <ma0@contraslash.com>
2017-06-22 13:24:43 -0500

    modified join swarm

--- join_swarm.sh
+++ join_swarm.sh
@@ -2,5 +2,5 @@ for ((i=1; i<=$NUM_WORKERS; i++))
 do
 echo "Joining the swarm in the node ${SWARM_WORKERS_PREFIX}${i}"
 eval $(docker-machine env ${SWARM_WORKERS_PREFIX}${i})
-docker swarm join     --token ${TOKEN}     ${MANAGER_IP}:2377
+docker swarm join --token ${TOKEN} --listen-addr $(docker-machine ip ${SWARM_WORKERS_PREFIX}${i}) --advertise-addr $(docker-machine ip ${SWARM_WORKERS_PREFIX}${i})  ${MANAGER_IP}:2377
 done
